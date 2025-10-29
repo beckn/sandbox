3a72721a36c53bcf61f16f81e59936c84918434f
shreyvishal <shreya.vishal@eminds.ai>
2025-10-30 00:34:53 +0530

    Added: Final Changes

--- src/webhook/controller.ts
+++ src/webhook/controller.ts
@@ -11,86 +11,281 @@ import on_rating_response from "./jsons/beckn.one:energy:ev-charging/response/on
 import on_support_response from "./jsons/beckn.one:energy:ev-charging/response/on_support.json";
 
 export const onSelect = (req: Request, res: Response) => {
-  const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
-  console.log(fullUrl, req.get("referer"));
-  const origin = req.get("origin") || req.get("referer") || "Unknown";
-  console.log("Request came from:", origin);
-
-  const ip =
-    req.headers["x-forwarded-for"] || req.socket.remoteAddress || "Unknown";
-  console.log("Caller IP:", ip);
   const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
   (async () => {
     try {
-      switch (context.action) {
-        case "select":
-          on_select_response.context = {...context, action: "on_select"};
-          const select_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_select`,
-            on_select_response
-          );
-          console.log(select_data.data);
-          break;
-        case "init":
-          on_init_response.context = {...context, action: "on_init"};
-          const init_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_init`,
-            on_init_response
-          );
-          break;
-        case "confirm":
-          on_confirm_response.context = {...context, action: "on_confirm"};
-          const confirm_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_confirm`,
-            on_confirm_response
-          );
-          break;
-        case "status":
-          on_status_response.context = {...context, action: "on_status"};
-          const status_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_status`,
-            on_status_response
-          );
-          break;
-        case "track":
-          on_track_response.context = {...context, action: "on_track"};
-          const track_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_track`,
-            on_track_response
-          );
-          break;
-        case "cancel":
-          on_cancel_response.context = {...context, action: "on_cancel"};
-          const cancel_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_cancel`,
-            on_cancel_response
-          );
-          break;
-        case "update":
-          on_update_response.context = {...context, action: "on_update"};
-          const update_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_update`,
-            on_update_response
-          );
-          break;
-        case "rating":
-          on_rating_response.context = {...context, action: "on_rating"};
-          const rating_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_rating`,
-            on_rating_response
-          );
-          break;
-        case "support":
-          on_support_response.context = {...context, action: "on_support"};
-          const support_data = await axios.post(
-            `http://localhost:8081/bpp/caller/on_support`,
-            on_support_response
-          );
-          break;
-      }
+      on_select_response.context = { ...context, action: "on_select" };
+      console.log(
+        "Triggering On Select response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_select`
+      );
+      const select_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_select`,
+        on_select_response
+      );
+      console.log("On Select api call response: ", select_data.data);
     } catch (error: any) {
-      console.log(error.message);
+      console.log(error);
+    } finally {
+      return;
     }
   })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onInit = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
+  (async () => {
+    try {
+      on_init_response.context = { ...context, action: "on_init" };
+      console.log(
+        "Triggering On Init response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_init`
+      );
+      const init_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_init`,
+        on_init_response
+      );
+      console.log("On Init api call response: ", init_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onConfirm = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  (async () => {
+    try {
+      on_confirm_response.context = { ...context, action: "on_confirm" };
+      console.log(
+        "Triggering On Confirm response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_confirm`
+      );
+      const confirm_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_confirm`,
+        on_confirm_response
+      );
+      console.log("On Confirm api call response: ", confirm_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onStatus = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  (async () => {
+    try {
+      on_status_response.context = { ...context, action: "on_status" };
+      console.log(
+        "Triggering On Status response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_status`
+      );
+      const status_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_status`,
+        on_status_response
+      );
+      console.log("On Status api call response: ", status_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onUpdate = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
+  (async () => {
+    try {
+      on_update_response.context = { ...context, action: "on_update" };
+      console.log(
+        "Triggering On Update response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_update`
+      );
+      const update_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_update`,
+        on_update_response
+      );
+      console.log("On Update api call response: ", update_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onRating = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  (async () => {
+    try {
+      on_rating_response.context = { ...context, action: "on_rating" };
+      console.log(
+        "Triggering On Rating response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_rating`
+      );
+      const rating_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_rating`,
+        on_rating_response
+      );
+      console.log("On Rating api call response: ", rating_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onSupport = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
+  (async () => {
+    try {
+      on_support_response.context = { ...context, action: "on_support" };
+      console.log(
+        "Triggering On Support response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_support`
+      );
+      const support_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_support`,
+        on_support_response
+      );
+      console.log("On Support api call response: ", support_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onTrack = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  (async () => {
+    try {
+      on_track_response.context = { ...context, action: "on_track" };
+      console.log(
+        "Triggering On Track response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_track`
+      );
+      const track_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_track`,
+        on_track_response
+      );
+      console.log("On Track api call response: ", track_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const onCancel = (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  (async () => {
+    try {
+      on_cancel_response.context = { ...context, action: "on_cancel" };
+      console.log(
+        "Triggering On Cancel response to:",
+        `${full_bpp_url.origin}/bpp/caller/on_cancel`
+      );
+      const cancel_data = await axios.post(
+        `${full_bpp_url.origin}/bpp/caller/on_cancel`,
+        on_cancel_response
+      );
+      console.log("On Cancel api call response: ", cancel_data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      return;
+    }
+  })();
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const triggerOnStatus = async (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
+  try {
+    console.log(
+      "Triggering On Status response to:",
+      `${full_bpp_url.origin}/bpp/caller/on_status`
+    );
+    const status_data = await axios.post(
+      `${full_bpp_url.origin}/bpp/caller/on_status`,
+      { context, message }
+    );
+    console.log("On Status api call response: ", status_data.data);
+  } catch (error: any) {
+    console.log(error);
+  }
+
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const triggerOnUpdate = async (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+  try {
+    console.log(
+      "Triggering On Update response to:",
+      `${full_bpp_url.origin}/bpp/caller/on_update`
+    );
+    const update_data = await axios.post(
+      `${full_bpp_url.origin}/bpp/caller/on_update`,
+      { context, message }
+    );
+    console.log("On Update api call response: ", update_data.data);
+  } catch (error: any) {
+    console.log(error);
+  }
+  return res.status(200).json({ message: { ack: { status: "ACK" } } });
+};
+
+export const triggerOnCancel = async (req: Request, res: Response) => {
+  const { context, message }: { context: any; message: any } = req.body;
+  const full_bpp_url = new URL(context.bpp_uri);
+
+  try {
+    console.log(
+      "Triggering On Cancel response to:",
+      `${full_bpp_url.origin}/bpp/caller/on_cancel`
+    );
+    const cancel_data = await axios.post(
+      `${full_bpp_url.origin}/bpp/caller/on_cancel`,
+      { context, message }
+    );
+    console.log("On Cancel api call response: ", cancel_data.data);
+  } catch (error: any) {
+    console.log(error);
+  }
   return res.status(200).json({ message: { ack: { status: "ACK" } } });
 };
--- src/webhook/routes.ts
+++ src/webhook/routes.ts
@@ -1,10 +1,36 @@
 import { Router, Request, Response } from "express";
-import { onSelect } from "./controller";
+import {
+  onSelect,
+  onInit,
+  onConfirm,
+  onStatus,
+  onCancel,
+  onUpdate,
+  onRating,
+  onSupport,
+  onTrack,
+  triggerOnStatus,
+  triggerOnCancel,
+  triggerOnUpdate
+} from "./controller";
 
 export const webhookRoutes = () => {
   const router = Router();
 
   router.post("/select", onSelect);
+  router.post("/init", onInit);
+  router.post("/confirm", onConfirm);
+  router.post("/status", onStatus);
+  router.post("/cancel", onCancel);
+  router.post("/update", onUpdate);
+  router.post("/rating", onRating);
+  router.post("/support", onSupport);
+  router.post("/track", onTrack);
+
+  // Unsolicited triggering routes
+  router.post("/trigger/on_status", triggerOnStatus);
+  router.post("/trigger/on_cancel", triggerOnCancel);
+  router.post("/trigger/on_update", triggerOnUpdate);
 
   return router;
 };
